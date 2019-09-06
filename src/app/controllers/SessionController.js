0090dcb009570e19542cd8d076bbd52c82fee2ed
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2019-09-05 23:57:18 -0300

    First Commit

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
+
+    const { id, name, user_uuid } = user;
+
+    // The MD5 hash for desafio02bootcamp is : 6062e63ff98a266e87bd8dfaae0383d6
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+        user_uuid,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
